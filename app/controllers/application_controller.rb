7b00efa0def1e543d019250dd900cf823fda27fc
Maxime Lerasle <m.lerasle@enten-itc.ch>
2015-04-20 16:36:31 +0200

    Fix devise redirect issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :admin_user
+  after_filter :store_location
 
   def admin_user
     @admin_user ||= User.where(email: ENV["admin_email"]).first
@@ -15,5 +16,22 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me, :avatar) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :avatar) }
+  end
+  
+  private
+  
+  def store_location
+    return unless request.get? 
+    if (!request.fullpath.match("/users") && !request.xhr?)
+      session[:previous_url] = request.fullpath if request.format == "text/html" || request.content_type == "text/html"
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+  
+  def after_sign_out_path_for(resource)
+    session[:previous_url] || root_path
   end
 end
