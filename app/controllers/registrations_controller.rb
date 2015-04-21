1f39111911038ad3ee7cb879a273bcfd0c5c8036
Maxime Lerasle <m.lerasle@enten-itc.ch>
2015-04-21 14:06:02 +0200

    Improve user destroy

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,9 +6,9 @@ class RegistrationsController < Devise::RegistrationsController
     end
     resource.destroy
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
-    # set_flash_message :notice, :destroyed if is_flashing_format?
+    set_flash_message :alert, :destroyed if is_flashing_format?
     yield resource if block_given?
-    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
+    respond_with_navigational(resource){ redirect_to new_user_registration_path }
   end
 
   private
