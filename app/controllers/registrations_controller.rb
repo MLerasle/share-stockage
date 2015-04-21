c8be2a801e048732dacf777fe67a3bdafcfc09e2
Maxime Lerasle <m.lerasle@enten-itc.ch>
2015-04-21 15:26:04 +0200

    Add first and last name to users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -14,11 +14,11 @@ class RegistrationsController < Devise::RegistrationsController
   private
  
   def sign_up_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation, :accept_cgu, :avatar)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :accept_cgu, :avatar, :first_name, :last_name)
   end
  
   def account_update_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password, :avatar)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password, :avatar, :first_name, :last_name)
   end
   
   protected
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421103946) do
+ActiveRecord::Schema.define(version: 20150421131218) do
 
   create_table "adverts", force: true do |t|
     t.string   "address"
@@ -150,6 +150,8 @@ ActiveRecord::Schema.define(version: 20150421103946) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
