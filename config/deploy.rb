5d928a202941e266134f716c81c4a3ca52452186
Maxime Lerasle <m.lerasle@enten-itc.ch>
2015-07-30 13:28:38 +0200

    Fix in capistrano receipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -69,7 +69,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
+      execute "sudo /etc/init.d/nginx restart"
     end
   end
 
