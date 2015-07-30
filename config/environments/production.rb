26d946cabd6ed0a9ffc31e5e0f3e796d7b65e717
Maxime Lerasle <m.lerasle@enten-itc.ch>
2015-07-30 19:45:50 +0200

    Fix?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'sharestockage.ch', protocol: 'http' }
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     address: "mail.gandi.net",
     port: 587,
