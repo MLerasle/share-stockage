ee9591eaa8ae14be49bdceaa3bcbf5b7db56f2ee
Maxime Lerasle <m.lerasle@enten-itc.ch>
2016-03-06 12:12:17 +0100

    Fix map link bug

--- app/controllers/adverts_controller.rb
+++ app/controllers/adverts_controller.rb
@@ -9,7 +9,7 @@ class AdvertsController < ApplicationController
     @adverts = @adverts.for_filter(params[:filter]) unless params[:filter].blank?
     my_adverts_address = []
     @adverts.each do |a|
-      my_adverts_address << { address: a.full_address, data: a.full_address, tag: "/adverts/#{a.slug}" }
+      my_adverts_address << { address: a.full_address, data: a.full_address, tag: "/garde-meuble/#{a.slug}" }
     end
     @my_json_address = my_adverts_address.to_json
     render layout: 'search_adverts'
